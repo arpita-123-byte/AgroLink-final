4900bec3eb203f282b4d8c8abc14d81234336b03
arpita-123-byte <arpitabansal321@gmail.com>
2025-07-14 18:50:52 +0530

    Fix: Add dynamic Google OAuth callback URL

--- server.js
+++ server.js
@@ -96,7 +96,8 @@ mongoose
         {
           clientID: process.env.GOOGLE_CLIENT_ID,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-          callbackURL: "/auth/google/callback",
+          callbackURL: process.env.GOOGLE_CALLBACK_URL,
+
         },
         async (accessToken, refreshToken, profile, done) => {
           const existingUser = await User.findOne({ email: profile.emails[0].value });
